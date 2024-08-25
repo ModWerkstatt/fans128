779d72e444af2dde1016d6792e89042d1d40451d
Florian Thiel <flth@flth.de>
2024-08-25 22:07:09 +0200

    initial

---
+++ mod.lua
@@ -0,0 +1,59 @@
+﻿function data()
+return {
+	info = {
+		minorVersion = 0,
+		severityAdd = "NONE",
+		severityRemove = "WARNING",
+		name = _("mod_name"),
+		description = _("mod_desc"),
+		authors = {
+		    {
+		        name = "ModWerkstatt",
+		        role = "CREATOR",
+		    },
+		},
+		tags = { "europe", "waggon", "schuettgut", "deutschland", "germany", "UIC", "gueterwagen", "db" },
+		tfnetId = { },
+		minGameVersion = 0,
+		dependencies = { },
+		url = { "" },
+
+        params = {
+			{
+				key = "fans128fake",
+				name = _("Fake_fans128_wagen"),
+				values = { "No", "Yes", },
+				tooltip = _("option_fake_fans128_desc"),
+				defaultIndex = 0,
+			},
+        },
+	},
+	options = {
+	},
+	
+	runFn = function (settings, modParams)
+	
+        local hidden = {
+			["DBCargo_fake.mdl"] = true,
+			["handbremse_DBCargo_fake.mdl"] = true,
+			["handbremse_Railion_fake.mdl"] = true,
+            ["Railion_fake.mdl"] = true,   
+        }
+
+		local modelFilter = function(fileName, data)
+			local modelName = fileName:match('/Fans128_([^/]*.mdl)')
+			return (modelName==nil or hidden[modelName]~=true)
+		end
+
+        if modParams[getCurrentModId()] ~= nil then
+			local params = modParams[getCurrentModId()]
+			if params["fans128fake"] == 0 then
+				addFileFilter("model/vehicle", modelFilter)
+			end
+		else
+			addFileFilter("model/vehicle", modelFilter)
+		end
+
+	end
+	}
+end
---
+++ strings.lua
@@ -0,0 +1,40 @@
+function data()
+return {
+	en = {
+
+		["mod_name"] = "DB Fans 128 Bogie bulk tipping wagon",
+		["mod_desc"] = "The two-trough two-way box tipper is designed for transporting bulk goods that are not sensitive to moisture. The wagon has an electro-hydraulic tipping drive and a programmable logic controller. For unloading, it has a power supply independent of external energy sources. Mostly, the fans 128 are used in block trains.",
+
+        ["Fans128_menu"] = "Fans 128",
+		["Fans128_dbcargo_name"] = "Fans 128 DB Cargo",
+        ["Fans128_railion_name"] = "Fans 128 Railion DB Logistics",
+		["Fans128_desc"] = "The two-trough two-way box tipper is designed for transporting bulk goods that are not sensitive to moisture. Mostly, the fans 128 are used in block trains.",
+        ["mit_handbremse"] = "(mit Feststellbremse)",
+        ["mit handbremse_desc"] = "The trolley is equipped with a hand wheel for the parking brake that can be operated from the ground.",
+		
+        ["Fans128_menu_fake"] = "Fans 128 Fake",
+		["Fake_fans128_wagen"] = "Enable fans 128 fake versions?",
+        ["option_fake_fans128_desc"] = "Should the fake versions of fans 128 be available in game?",
+	    ["fake"] = "Fake",
+		
+	},
+	de = {
+
+		["mod_name"] = "Fans 128 Drehgestell-Schüttgutkippwagen",
+		["mod_desc"] = "Der zweimuldige Zweiseiten-Kastenkipper ist für den Transport von feuchtigkeits-unempfindlichen Schüttgütern vorgesehen. Der Wagen besitzt einen elektrohydraulischen Kippantrieb und eine speicherprogrammierbare Steuerung. Zum Entladen besitzt er eine von äußeren Energiequellen unabhängige Spannungsversorgung. Meist werden die Fans 128 in Ganzzügen eingesetzt.",
+
+        ["Fans128_menu"] = "Fans 128",
+		["Fans128_dbcargo_name"] = "Fans 128 DB Cargo",
+        ["Fans128_railion_name"] = "Fans 128 Railion DB Logistics",
+		["Fans128_desc"] = "Der zweimuldige Zweiseiten-Kastenkipper ist für den Transport von feuchtigkeits-unempfindlichen Schüttgütern vorgesehen. Meist werden die Fans 128 in Ganzzügen eingesetzt.",
+        ["mit_handbremse"] = "(mit Feststellbremse)",
+        ["mit handbremse_desc"] = "Der Wagen ist mit einem vom Boden aus zu bedienenden Handrad für die Feststellbremse ausgestattet.",
+		
+        ["Fans128_menu_fake"] = "Fans 128 Fake",
+		["Fake_fans128_wagen"] = "Fans 128 Fake-Versionen aktivieren?",
+        ["option_fake_fans128_desc"] = "Sollen die Fake-Versionen der Fans 128 verfügbar sein im Spiel?",
+	    ["fake"] = "Fake",
+
+	},
+}
+end
